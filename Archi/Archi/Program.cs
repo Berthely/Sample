8b9d8c24d60fe8bbe8befce8143b31c1ed2a83e9
elioberthely <elioberthely@hotmail.com>
2015-01-29 20:13:37 -0800

    tes

---
+++ Archi/Archi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Texto
+{
+    class Program
+    {
+      
+
+        static void Main(string[] args)
+        {
+            
+            
+            Texto Frase = new Texto();
+            Frase.declaracion();
+                                                                                
+        }      
+
+        
+    }
+}
---
+++ Archi/Archi/Texto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Texto
+{
+    //construir un metodo que con una linea de texto ubique una palabra "x" y que diga
+    //en que posicion empieza
+    
+    class Texto
+    {
+        public void declaracion()
+        {
+            string frase;
+            string buscar;
+            int i;
+            int j;
+            int w;
+            frase = "perro que come huevo";
+            buscar = "huevo";
+                            
+            
+            Console.WriteLine("Programa que indica la posicion de la palabra");
+            Console.WriteLine();
+            Console.ReadLine();
+
+            for (i=0; i<frase.Length; i++)
+
+            Console.Write(frase[i] + " ");
+            Console.WriteLine();
+            Console.ReadLine();
+
+            for (j=0; j<buscar.Length; j++)
+            Console.Write(buscar[j]+" ");
+            Console.ReadLine();
+                     
+            for (w=0; w<=buscar.Length; w++)                
+                    if (buscar[w] == frase[w])
+
+                    {                                                
+                        Console.WriteLine(frase[w]);
+                        Console.ReadLine();
+                    }
+                
+            
+        }
+
+    }
+}
